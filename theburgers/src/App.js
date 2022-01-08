4f8fa09693de3c2dce79264e5207c3837eb0e021
yudzeshka <yudzeshka@gmail.com>
2022-01-08 23:56:40 +0300

    added Router

--- theburgers/src/App.js
+++ theburgers/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Burgers from "./components/Pages/Burgers";
 
 function App() {
   return (
     <div className="wrapper">
+      <Routes>
+        <Route path="/burgers" exact element={<Burgers />}></Route>
+      </Routes>
       <Header />
       <div className="content">
         <div className="textWrapper">
--- theburgers/src/components/Header.js
+++ theburgers/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div className="header">
       <nav className="flex items-center list-none">
         <li>HOME</li>
-        <li>BURGERS</li>
+        <li>
+          <Link to="/burgers">BURGERS</Link>
+        </li>
         <li>DESERTS</li>
         <li>BREAKFESTS</li>
         <li>DRINKS</li>
