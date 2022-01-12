6616e427f7c8fdfdaa493127e6dd903bbda5f9fa
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-01-12 16:54:41 +0300

    styles fixed

--- theburgers/src/components/Cart.js
+++ theburgers/src/components/Cart.js
@@ -5,8 +5,8 @@ export default function Cart({ imgSrc, description, dishName, price }) {
     <div className="cart">
       <div className="relative group">
         <img className="dishImg" src={imgSrc} alt="#" />
-        <div className="absolute w-full h-full top-0 left-0 bg-black/50 hidden group-hover:flex items-center  hover:transition-all text-white">
-          <p className="text-white text-center">{description}</p>
+        <div className="absolute w-full h-full top-0 left-0 bg-black/70 hidden group-hover:flex items-center  hover:transition-all text-white">
+          <p className="text-white text-center font-bold">{description}</p>
         </div>
       </div>
       <div className="dishName block text-center font-bold">{dishName}</div>
