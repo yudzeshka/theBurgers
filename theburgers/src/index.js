c8c1998c953c258cf2d7aba8adcde7d57d90f2bb
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-01-14 09:33:16 +0300

    Revert "bugs fixed"
    This reverts commit c400bc755ed54fcea67ba9bba981cc5e4240e402.

--- theburgers/src/index.js
+++ theburgers/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
