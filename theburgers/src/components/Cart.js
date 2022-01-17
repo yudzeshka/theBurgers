54d92294f6fcd26e7b82502cb1e4cddba2e1aac9
yudzeshka <yudzeshka@gmail.com>
2022-01-17 23:38:29 +0300

    changed styles

--- theburgers/src/components/Cart.js
+++ theburgers/src/components/Cart.js
@@ -5,11 +5,11 @@ export default function Cart({ imgSrc, description, dishName, price }) {
     <div className="cart">
       <div className="relative group">
         <img className="dishImg" src={imgSrc} alt="#" />
-        <div className="absolute w-full h-full top-0 left-0 bg-black/70 hidden group-hover:flex items-center text-white">
+        <div className="absolute w-full h-full top-0 left-0 bg-black/80 opacity-0 group-hover:opacity-100 transition-all duration-400 ease-out flex items-center text-white">
           <p className="text-white text-center font-bold">{description}</p>
         </div>
       </div>
-      <div className="dishName block text-center font-bold">{dishName}</div>
+      <div className="dishName block text-center font-bold ">{dishName}</div>
       <div className="flex flex-row justify-between items-center m-2">
         <b>{price}</b>
         <button>Add to card</button>
